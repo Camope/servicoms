14fe93083627bf2af69480767f716179eb0ce758
Carlos Moreno <camorpe@gmail.com>
2023-04-27 17:16:57 +0200

    listado de comisiones

--- frontend/src/components/ListadoComisiones.vue
+++ frontend/src/components/ListadoComisiones.vue
@@ -27,87 +27,12 @@ export default {
     this.initFilters();
   },
   mounted() {
-    ProductService.getProducts().then((data) => (this.products = data))
+   // ProductService.getProducts().then((data) => (this.products = data))
     this.listaComisiones = this.getComisiones()
     this.listaComisiones.forEach(element => element.estado = element.id % 3)
   },
   methods: {
-    openNew() {
-      this.product = {};
-      this.submitted = false;
-      this.productDialog = true;
-    },
-    hideDialog() {
-      this.productDialog = false;
-      this.submitted = false;
-    },
-    saveProduct() {
-      this.submitted = true;
-
-      if (this.product.name.trim()) {
-        if (this.product.id) {
-          this.product.inventoryStatus = this.product.inventoryStatus.value ? this.product.inventoryStatus.value : this.product.inventoryStatus;
-          this.products[this.findIndexById(this.product.id)] = this.product;
-          this.$toast.add({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
-        }
-        else {
-          this.product.id = this.createId();
-          this.product.code = this.createId();
-          this.product.image = 'product-placeholder.svg';
-          this.product.inventoryStatus = this.product.inventoryStatus ? this.product.inventoryStatus.value : 'INSTOCK';
-          this.products.push(this.product);
-          this.$toast.add({ severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000 });
-        }
-
-        this.productDialog = false;
-        this.product = {};
-      }
-    },
-    editProduct(product) {
-      this.product = { ...product };
-      this.productDialog = true;
-    },
-    confirmDeleteProduct(product) {
-      this.product = product;
-      this.deleteProductDialog = true;
-    },
-    deleteProduct() {
-      this.products = this.products.filter(val => val.id !== this.product.id);
-      this.deleteProductDialog = false;
-      this.product = {};
-      this.$toast.add({ severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
-    },
-    findIndexById(id) {
-      let index = -1;
-      for (let i = 0; i < this.products.length; i++) {
-        if (this.products[i].id === id) {
-          index = i;
-          break;
-        }
-      }
-
-      return index;
-    },
-    createId() {
-      let id = '';
-      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      for (var i = 0; i < 5; i++) {
-        id += chars.charAt(Math.floor(Math.random() * chars.length));
-      }
-      return id;
-    },
-    exportCSV() {
-      this.$refs.dt.exportCSV();
-    },
-    confirmDeleteSelected() {
-      this.deleteProductsDialog = true;
-    },
-    deleteSelectedProducts() {
-      this.products = this.products.filter(val => !this.selectedProducts.includes(val));
-      this.deleteProductsDialog = false;
-      this.selectedProducts = null;
-      this.$toast.add({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
-    },
+    
     initFilters() {
       this.filters = {
         'global': { value: null, matchMode: FilterMatchMode.CONTAINS },
@@ -116,13 +41,13 @@ export default {
     getStatusLabel(status) {
       switch (status) {
         case 0:
-          return 'success';
+          return { value: 'En plazo', severity: 'success' }
 
         case 1:
-          return 'warning';
+          return { value: 'Finalizando', severity: 'warning' }
 
         case 2:
-          return 'danger';
+          return { value: 'Cerrada', severity: 'danger' }
 
         default:
           return null;
@@ -158,109 +83,10 @@ export default {
         <Column field="localidad" header="Localidad" sortable style="width:10rem"></Column>
         <Column field="estado" header="Estado" sortable style="min-width:12rem">
           <template #body="slotProps">
-            <Tag :value="slotProps.data.estado" :severity="getStatusLabel(slotProps.data.estado)" />
+            <Tag :value="getStatusLabel(slotProps.data.estado).value" :severity="getStatusLabel(slotProps.data.estado).severity" />
           </template>
         </Column>
-        <!-- <Column :exportable="false" style="min-width:8rem">
-          <template #body="slotProps">
-            <Button icon="pi pi-pencil" outlined rounded class="mr-2" @click="editProduct(slotProps.data)" />
-            <Button icon="pi pi-trash" outlined rounded severity="danger" @click="confirmDeleteProduct(slotProps.data)" />
-          </template>
-        </Column> -->
       </DataTable>
     </div>
-
-    <Dialog v-model:visible="productDialog" :style="{ width: '450px' }" header="Product Details" :modal="true"
-      class="p-fluid">
-      <img v-if="product.image" :src="`https://primefaces.org/cdn/primevue/images/product/${product.image}`"
-        :alt="product.image" class="block m-auto pb-3" />
-      <div class="field">
-        <label for="name">Name</label>
-        <InputText id="name" v-model.trim="product.name" required="true" autofocus
-          :class="{ 'p-invalid': submitted && !product.name }" />
-        <small class="p-error" v-if="submitted && !product.name">Name is required.</small>
-      </div>
-      <div class="field">
-        <label for="description">Description</label>
-        <Textarea id="description" v-model="product.description" required="true" rows="3" cols="20" />
-      </div>
-
-      <div class="field">
-        <label for="inventoryStatus" class="mb-3">Inventory Status</label>
-        <Dropdown id="inventoryStatus" v-model="product.inventoryStatus" :options="statuses" optionLabel="label"
-          placeholder="Select a Status">
-          <template #value="slotProps">
-            <div v-if="slotProps.value && slotProps.value.value">
-              <Tag :value="slotProps.value.value" :severity="getStatusLabel(slotProps.value.label)" />
-            </div>
-            <div v-else-if="slotProps.value && !slotProps.value.value">
-              <Tag :value="slotProps.value" :severity="getStatusLabel(slotProps.value)" />
-            </div>
-            <span v-else>
-              {{ slotProps.placeholder }}
-            </span>
-          </template>
-        </Dropdown>
-      </div>
-
-      <div class="field">
-        <label class="mb-3">Category</label>
-        <div class="formgrid grid">
-          <div class="field-radiobutton col-6">
-            <RadioButton id="category1" name="category" value="Accessories" v-model="product.category" />
-            <label for="category1">Accessories</label>
-          </div>
-          <div class="field-radiobutton col-6">
-            <RadioButton id="category2" name="category" value="Clothing" v-model="product.category" />
-            <label for="category2">Clothing</label>
-          </div>
-          <div class="field-radiobutton col-6">
-            <RadioButton id="category3" name="category" value="Electronics" v-model="product.category" />
-            <label for="category3">Electronics</label>
-          </div>
-          <div class="field-radiobutton col-6">
-            <RadioButton id="category4" name="category" value="Fitness" v-model="product.category" />
-            <label for="category4">Fitness</label>
-          </div>
-        </div>
-      </div>
-
-      <div class="formgrid grid">
-        <div class="field col">
-          <label for="price">Price</label>
-          <InputNumber id="price" v-model="product.price" mode="currency" currency="USD" locale="en-US" />
-        </div>
-        <div class="field col">
-          <label for="quantity">Quantity</label>
-          <InputNumber id="quantity" v-model="product.quantity" integeronly />
-        </div>
-      </div>
-      <template #footer>
-        <Button label="Cancel" icon="pi pi-times" text @click="hideDialog" />
-        <Button label="Save" icon="pi pi-check" text @click="saveProduct" />
-      </template>
-    </Dialog>
-
-    <Dialog v-model:visible="deleteProductDialog" :style="{ width: '450px' }" header="Confirm" :modal="true">
-      <div class="confirmation-content">
-        <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
-        <span v-if="product">Are you sure you want to delete <b>{{ product.name }}</b>?</span>
-      </div>
-      <template #footer>
-        <Button label="No" icon="pi pi-times" text @click="deleteProductDialog = false" />
-        <Button label="Yes" icon="pi pi-check" text @click="deleteProduct" />
-      </template>
-    </Dialog>
-
-    <Dialog v-model:visible="deleteProductsDialog" :style="{ width: '450px' }" header="Confirm" :modal="true">
-      <div class="confirmation-content">
-        <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
-        <span v-if="product">Are you sure you want to delete the selected products?</span>
-      </div>
-      <template #footer>
-        <Button label="No" icon="pi pi-times" text @click="deleteProductsDialog = false" />
-        <Button label="Yes" icon="pi pi-check" text @click="deleteSelectedProducts" />
-      </template>
-    </Dialog>
   </div>
 </template>
