590b7c43bfc4e7f80cfa631f4d9d0b9bd79c38c3
Carlos Moreno <camorpe@gmail.com>
2023-05-05 21:01:57 +0200

    Barra de navegacion fix #11

--- frontend/src/components/BarraNavegacion.vue
+++ frontend/src/components/BarraNavegacion.vue
@@ -8,7 +8,9 @@ export default {
   emits: [],
   data() {
     return {
-     
+      isLoggedIn: false,
+      itemVisible: false,
+      isAdmin: false
     }
   },
   created() {
@@ -17,15 +19,25 @@ export default {
 
   },
   computed: {
-
   },
   methods: {
     //    ...mapActions(useComisionesStore, ['getComisiones', 'setComision']),
     muestraMenuLogout(event) {
       this.$refs.menu.toggle(event);
     },
-    linkAdmin() {
+    login(role) {
       console.log("click en admin")
+      this.isLoggedIn = true
+      this.isAdmin = (role == "admin")
+    },
+    logout() {
+      this.isLoggedIn = false
+    },
+    goHome() {
+      this.$router.push({ name: "comisiones" });
+    },
+    misComisiones() {
+      
     }
   }
 }
@@ -40,35 +52,83 @@ export default {
       </div>
 
       <div class="p-menubar-end">
-        <!-- <span>Carlos Moreno</span> -->
-        <!-- <Button icon="pi pi-user" severity="info" rounded outlined @click="muestraMenuLogout" />
-        <Menu ref="menu" :model="items" :popup="true" /> -->
-        <!-- <div class="dropdown"> -->
         <a class="" href="#" role="button" data-bs-toggle="dropdown">
-          <Button icon="pi pi-user" severity="info" rounded outlined />
+          <Button icon="pi pi-user" severity="warning" rounded />
         </a>
 
-        <ul class="dropdown-menu dropdown-menu-end p-menu-overlay p-menu p-component">
-          <li class="p-submenu-header" role="none">Login</li>
-          <li><hr class="dropdown-divider"></li>
-          <li class="p-menuitem" @click="linkAdmin">
-            <div class="p-menuitem-content">
-              <a class="p-menuitem-link" tabindex="-1">
-                <span class="p-menuitem-icon pi pi-fw pi-user"></span>
-                <span class="p-menuitem-text">Admin</span>
-              </a>
-            </div>
-          </li>
-          <li class="p-menuitem">
-            <div class="p-menuitem-content">
-              <a class="p-menuitem-link" tabindex="-1">
-                <span class="p-menuitem-icon pi pi-fw pi-users"></span>
-                <span class="p-menuitem-text">User</span>
-              </a>
-            </div>
-          </li>
+        <div class="dropdown-menu dropdown-menu-end p-menu-overlay p-menu p-component">
+          <ul v-if="!isLoggedIn">
+            <li class="p-submenu-header flex" role="none">
+              <div class="">
+                <i class="pi pi-sign-in mr-3" style="font-size: 1.5rem" />
+              </div>
+              <div class="flex flex-column justify-content-center">
+                <span class="font-bold">Login</span>
+              </div>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li class="p-menuitem" @click="login('admin')">
+              <div class="p-menuitem-content">
+                <a class="p-menuitem-link" tabindex="-1">
+                  <span class="p-menuitem-icon pi pi-fw pi-user"></span>
+                  <span class="p-menuitem-text">Admin</span>
+                </a>
+              </div>
+            </li>
+            <li class="p-menuitem" @click="login('notAdmin')">
+              <div class="p-menuitem-content">
+                <a class="p-menuitem-link" tabindex="-1">
+                  <span class="p-menuitem-icon pi pi-fw pi-users"></span>
+                  <span class="p-menuitem-text">User</span>
+                </a>
+              </div>
+            </li>
+          </ul>
 
-        </ul>
+          <ul v-if="isLoggedIn">
+            <li class="p-submenu-header flex" role="none">
+              <div class="">
+                <i class="pi pi-user mr-3" style="font-size: 1.5rem" />
+              </div>
+              <div class="flex flex-column justify-content-center">
+                <span class="font-bold">Carlos Moreno</span>
+                <span class="text-sm">{{ isAdmin ? "Admin" : "Usuario" }}</span>
+              </div>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li class="p-menuitem" @click="misComisiones">
+              <div class="p-menuitem-content">
+                <a class="p-menuitem-link" tabindex="-1">
+                  <!-- <span class="p-menuitem-icon pi pi-fw pi-user"></span> -->
+                  <span class="p-menuitem-text">Mis Comisiones</span>
+                </a>
+              </div>
+            </li>
+            <li class="p-menuitem" @click="goHome">
+              <div class="p-menuitem-content">
+                <a class="p-menuitem-link" tabindex="-1">
+                  <!-- <span class="p-menuitem-icon pi pi-fw pi-users"></span> -->
+                  <span class="p-menuitem-text">Lista de comisiones</span>
+                </a>
+              </div>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li class="p-menuitem" @click="logout">
+              <div class="p-menuitem-content">
+                <a class="p-menuitem-link" tabindex="-1">
+                  <span class="p-menuitem-icon pi pi-fw pi-sign-out"></span>
+                  <span class="p-menuitem-text">Logout</span>
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -76,6 +136,7 @@ export default {
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Odibee+Sans&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Michroma&display=swap');
 
 .logo {
   display: flex;
@@ -83,7 +144,8 @@ export default {
 }
 
 .texto-logo {
-  font-family: 'Odibee Sans', cursive;
-  font-size: 2rem;
-}
-</style>
+  /* font-family: 'Odibee Sans', cursive; */
+  font-family: 'Michroma', sans-serif;
+  font-weight: 600;
+  font-size: 1.5rem;
+}</style>
