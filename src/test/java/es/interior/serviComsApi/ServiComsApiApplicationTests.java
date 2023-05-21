098be4592dd14857eaea0d4f8d7ab539dc10e8ba
Carlos Moreno <camorpe@gmail.com>
2023-05-21 21:08:15 +0200

    Configuracion entorno desarrollo API fix #27

---
+++ src/test/java/es/interior/serviComsApi/ServiComsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.interior.serviComsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiComsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
